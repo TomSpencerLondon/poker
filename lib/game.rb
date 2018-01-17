1ccdf3ba9aca3608406c377436a896980213cc3d
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-17 16:00:01 +0000

    Complete rules

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,63 @@
 class Game
 
   RANKING = [:A, :K, :Q, :J, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+  TRANSLATION = { A: 'Ace', K: 'King', Q: 'Queen', J: 'Jack' }
 
   def categorise(hand)
-    puts "Royal Flush" if royal_flush?(hand)
-    puts "Straight Flush" if straight_flush?(hand)
-    puts "Four of a Kind" if four_of_a_kind?(hand)
+    return "Royal Flush" if royal_flush?(hand)
+    return "Straight Flush" if straight_flush?(hand)
+    return "Four of a Kind" if four_of_a_kind?(hand)
+    return "Full House" if full_house?(hand)
+    return "Flush" if flush?(hand)
+    return "Straight" if straight?(hand)
+    return "Three of a Kind" if three_of_a_kind?(hand)
+    return "Two Pair" if two_pair?(hand)
+    return "One Pair" if one_pair?(hand)
+    "Your High Card is #{high_card(hand)}"
+  end
+
+  def high_card(hand)
+    indices = ranks(hand).map{ |rank| RANKING.index(rank)}.sort
+    high_index = RANKING[indices[0]]
+    return high_index.is_a?(Integer) ? high_index : TRANSLATION[high_index.to_sym]
+  end
+
+  def straight?(hand)
+    indices = ranks(hand).map{ |rank| RANKING.index(rank)}.sort
+    consecutive?(indices) && !same_suits?(hand)
+  end
+
+  def flush?(hand)
+    same_suits?(hand)
+  end
+
+  def full_house?(hand)
+    one_pair?(hand) && three_of_a_kind?(hand)
+  end
+
+  def one_pair?(hand)
+    RANKING.one? { |card| ranks(hand).count(card) == 2 }
+  end
+
+  def two_pair?(hand)
+    RANKING.find_all { |card| ranks(hand).count(card) == 2 }.count == 2
+  end
+
+  def three_of_a_kind?(hand)
+    RANKING.one? { |card| ranks(hand).count(card) == 3 }
   end
 
   def royal_flush?(hand)
-    ranks(hand) == [:A, :K, :Q, :J, 10] && same_suits?(hand) ? true : false
+    ranks(hand) == [:A, :K, :Q, :J, 10] && same_suits?(hand)
   end
 
   def straight_flush?(hand)
     indices = ranks(hand).map{ |rank| RANKING.index(rank)}.sort
-    consecutive?(indices) && same_suits?(hand) ? true : false
+    consecutive?(indices) && same_suits?(hand)
   end
 
   def four_of_a_kind?(hand)
-    array = ranks(hand)
-    array.select{ |x| ranks(hand).count(x) == 4 }.count == 4
+    RANKING.one? { |card| ranks(hand).count(card) == 4 }
   end
 
   def consecutive?(ranking_indices)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,20 +2,57 @@ require 'game'
 
 describe Game do
 
+  let(:output) { StringIO.new }
+
   describe "#categorise" do
-    it "returns a 'royal flush'" do
+    it "returns a 'Royal Flush'" do
       hand = [[:A, :H], [:K, :H], [:Q, :H], [:J, :H], [10, :H]]
       expect(subject.categorise(hand)).to eq("Royal Flush")
     end
 
-    it "returns a 'straight flush'" do
+    it "returns a 'Straight Flush'" do
       hand = [[9, :C], [8, :C], [7, :C], [6, :C], [5, :C]]
       expect(subject.categorise(hand)).to eq("Straight Flush")
     end
 
-    it "returns a 'four of a kind'" do
+    it "returns a 'Four of a Kind'" do
       hand = [[:J, :H], [:J, :C], [:J, :S], [:J, :D], [7, :C]]
       expect(subject.categorise(hand)).to eq("Four of a Kind")
+    end
+
+    it "returns a 'Full House'" do
+      hand = [[:J, :C], [:J, :D], [:J, :S], [6, :H], [6, :C]]
+      expect(subject.categorise(hand)).to eq("Full House")
+    end
+
+    it "returns a 'Three of a Kind'" do
+      hand = [[:J, :C], [:J, :D], [:J, :S], [6, :H], [2, :C]]
+      expect(subject.categorise(hand)).to eq("Three of a Kind")
+    end
+
+    it "returns a 'One Pair'" do
+      hand = [[:J, :C], [:J, :D], [:K, :S], [6, :H], [2, :C]]
+      expect(subject.categorise(hand)).to eq("One Pair")
+    end
+
+    it "returns a 'Two Pair'" do
+      hand = [[:J, :C], [:J, :D], [:K, :S], [:K, :H], [6, :C]]
+      expect(subject.categorise(hand)).to eq("Two Pair")
+    end
+
+    it "returns a 'Flush'" do
+      hand = [[5, :D], [8, :D], [10, :D], [:J, :D], [:A, :D]]
+      expect(subject.categorise(hand)).to eq("Flush")
+    end
+
+    it "returns a 'Straight'" do
+      hand = [[9, :H], [8, :C], [7, :C], [6, :C], [5, :C]]
+      expect(subject.categorise(hand)).to eq("Straight")
+    end
+
+    it "returns a 'High Card' and specifies which card is highest" do
+      hand = [[2, :H], [4, :S], [8, :H], [:Q, :C], [:A, :D]]
+      expect(subject.categorise(hand)).to eq("Your High Card is Ace")
     end
   end
 
